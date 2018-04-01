c69b708b20b2e62570c69f0cdbcae5c0f5532c25
IonutBuda <ionutbuda@live.com>
2018-04-01 12:15:45 +0300

    Created expand-collapse all widgets button.

--- exconwid.js
+++ exconwid.js
@@ -1,15 +1,15 @@
 var leftBar = document.getElementById("left-bar");
-var widgetMod = leftBar.getElementsByTagName("div");
+var widgets = leftBar.getElementsByTagName("div");
 var expandCollapseButton = leftBar.getElementsByTagName("button");
-expandCollapseButton.onclick= function () {
+expandCollapseButton.onclick = function() {
     if (expandCollapseButton.innerHTML == "Collapse all widgets") {
         for (var i = 0; i < widgetMod.length; i++) {
-            widgetMod[i].className = "widget collapsed";
+            widgets[i].className = "widget collapsed";
         }
         expandCollapseButton.innerHTML = "Expand all widgets";
     } else {
         for (var i = 0; i < widgetMod.length; i++) {
-            widgetMod[i].className = "widget";
+            widgets[i].className = "widget";
         }
         expandCollapseButton.innerHTML = "Collapse all widgets";
     }
