bb474a2802f1d02ea99b17145f573616d0ae2ff0
IonutBuda <ionutbuda@live.com>
2018-04-02 20:54:34 +0300

    dynamic html demo

---
+++ js/phonebook.js
@@ -0,0 +1,4 @@
+var row1 = '<tr><td>Buda</td><td>Ionut</td><td>0752386608</td></tr>'
+var row2 = '<tr><td>Nicolae</td><td>Matei</td><td>0324235235</td></tr>'
+
+$('#phone-book tbody').html(row1 + row2);
