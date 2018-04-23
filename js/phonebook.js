5bc6689affeaa5ed74139b478ce96e4493d4110c
IonutBuda <ionutbuda@live.com>
2018-04-23 13:26:23 +0300

    Created randomize link color button.

--- js/phonebook.js
+++ js/phonebook.js
@@ -3,9 +3,9 @@ function getRow(firstName, lastName, phone) {
 }
 
 var persons = [
-    ['Ionut', 'Buda', '0752319222'],
-    ['Nicolae', 'Matei', '0324235235'],
-    ['Raluca', 'Ignat', '0766766723']
+    ['Ionut', 'Buda', '752319222'],
+    ['Nicolae', 'Matei', '324235235'],
+    ['Raluca', 'Ignat', '766766723']
 ];
 var rows = '';
 
@@ -13,11 +13,28 @@ persons.forEach(function (person) {
     rows += getRow(person[0],person[1],person[2]);
 });
 
-function pentruFiecarePers(persons) {
-    persons.forEach(function (persons) {
-        console.info ("Salut" + person[0])
-    })
-}
+function paySalary(person) {
+    console.warn ('Primeste salariul: ' + person[2]);
+};
+
+function payBonus(person){
+  console.info("Bonus: " + (person[2]*10/100));
+};
+function forEach(persons, action) {
+    console.info("===JOB STARTED===");
+    for (var i = 0; i<persons.length; i++) {
+        var person = persons[i];
+        action(person);
+    }
+};
+
+forEach(persons,paySalary);
+forEach(persons,paySalary);
+forEach(persons,paySalary);
+forEach(persons,payBonus);
+
+persons.forEach(paySalary);
+persons.forEach(payBonus);
 
 $('#phone-book tbody').html(rows);
 
@@ -26,7 +43,8 @@ var person={
     prenume: "Nicolae",
     hasJob: true,
     sayHello: function() {
-        return "Salut, bine ai venit pe site-ul lui " + this.nume + " " + this.prenume;
+        return "Salut, bine ai venit pe site-ul lui "
+            + this.nume + " " + this.prenume;
     },
     sleep: function() {
         return this.sayHello()+": Yawn..."
