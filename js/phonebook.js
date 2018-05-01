2de27bb0f00b66016ecc81d242562615cdd860f1
IonutBuda <ionutbuda@live.com>
2018-05-01 11:21:18 +0300

    dynamic html demo

--- js/phonebook.js
+++ js/phonebook.js
@@ -1,21 +1,26 @@
 function getRow(firstName, lastName, phone) {
-    return '<tr><td>' + firstName + '</td><td>' + lastName + '</td><td>' + phone + '</td></tr>'
+    return '<tr>' +
+        '<td>' + firstName + '</td>' +
+        '<td>' + lastName + '</td>' +
+        '<td>' + phone + '</td>' +
+        '<td><a href="/tmp/remove-contact.html">x</a></td>' +
+        '</tr>'
 }
 
 var persons = [];
 
 $.get("js/mocks/phone-book.json").complete(function () {
-    console.warn("phone book complete",arguments);
+    console.warn("phone book complete", arguments);
 });
 
 console.info("Loading persons");
 
 $.ajax({
-    url: 'js/mocks/phone-book.json',
+    url: 'js/mocks/phone-book.json'
 
-        }).done(function (data) {
-            console.info("done...",data);
-            display(data);
+}).done(function (data) {
+    console.info("done...", data);
+    display(data);
 
 });
 
@@ -24,12 +29,19 @@ function display(persons) {
     var rows = '';
 
     function createRows(person) {
-        rows += getRow(person.firstName,person.lastName,person.phone);
+        rows += getRow(person.firstName, person.lastName, person.phone);
     };
 
 
-
     persons.forEach(createRows);
+
+    rows += '<tr>' +
+        '<td><input type="text" name="firstName" required placeholder="Enter first name"></td>' +
+        '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
+        '<td><input type="text" name="phone" required placeholder="Enter phone">' +
+        '<button type="submit">Add</button></td>' +
+        '</tr>';
+
 
 
     $('#phone-book tbody').html(rows);
