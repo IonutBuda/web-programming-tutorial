5b9fdff179aada497714fbf8e62c6b75cc878f79
IonutBuda <ionutbuda@live.com>
2018-05-01 12:37:13 +0300

    dynamic html demo

--- js/phonebook.js
+++ js/phonebook.js
@@ -3,7 +3,9 @@ function getRow(person) {
         '<td>' + person.firstName + '</td>' +
         '<td>' + person.lastName + '</td>' +
         '<td>' + person.phone + '</td>' +
-        `<td><a href="/tmp/remove-contact.html?id=${person.id}">x</a></td>` +
+        `<td>`
+        + `<a href="tmp/remove-contact.html?id=${person.id}">&#10008;</a>`
+        + "  " + `<a href="#" data-id="${person.id}" class="edit">&#9998;</a></td>` +
         '</tr>'
 }
 
@@ -39,11 +41,30 @@ function display(persons) {
         '<td><input type="text" name="firstName" required placeholder="Enter first name"></td>' +
         '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
         '<td><input type="text" name="phone" required placeholder="Enter phone"></td>' +
-        '<td><button type="submit">Add</button></td>' +
+        '<td><button type="submit" id="buton">Add</button></td>' +
         '</tr>';
 
 
 
     $('#phone-book tbody').html(rows);
 
+    $("#phone-book tbody a.edit").click(function () {
+        var id = this.attributes["data-id"].value;
+        // var id = $(this).attr("data-id");
+        // var id = $(this).data('id');
+        console.info("click on", this, id);
+
+        var editPerson = persons.find(function (person) {
+            console.log(person.firstName);
+            return person.id == id;
+        });
+        console.warn('edit', editPerson);
+
+        $('input[name=firstName]').val(editPerson.firstName);
+        $('input[name=lastName]').val(editPerson.lastName);
+        $('input[name=phone]').val(editPerson.phone);
+        document.getElementById("buton").innerText = "Save";
+    })
+
 }
+
